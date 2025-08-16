8e77857dc716072f1481f0c1b4edf06b5f80e206
unknown <asamidorice@gmail.com>
2025-08-16 14:09:44 +0300

    week2 updates

---
+++ week_2/dict.py
@@ -0,0 +1,106 @@
+#dictionaries
+#collection of multiple key/value pairs
+
+
+#creating a dictionary
+# purse = dict()
+# purse= ['money']=12
+# purse= ['candy']=50
+# purse= ['tissue']=60
+
+# print (purse)
+
+# purse = {}
+# purse ['money'] = 12
+# purse ['candy'] = 80
+# purse ['tissue'] = 30
+
+# print (purse)
+
+# student = dict ()
+
+# student ['name'] = 'dorice'
+# student ['age']  = 21
+# student ['course']= 'python'
+
+# print(student)
+
+# # create a dictionary in one line
+# dog = {}
+
+# dog ={'name':'Rodger', 'age':8}
+
+# # print(dog)
+
+# #change value stored in a specific index
+
+# dog['name'] = 'Tobby'
+
+# #print (dog)
+
+# # # get method to add default values
+
+# # dog.get('test','default')
+
+# # print(dog)
+
+# # pop()method to retrieve value of a key and delete
+# # subjects.popitem()
+# # print(subjects)
+
+# subjects= {}
+
+# subjects = {'english':89, 'maths': 96, 'science': 78}
+ 
+# subjects.get('physics' '67')
+# print(subjects)
+
+# subjects = {'english':89, 'maths': 96, 'science': 78}
+
+# list(subjects.keys())
+# print(subjects)
+
+# subjects = {'english':89, 'maths': 96, 'science': 78}
+
+# print(len(subjects))
+
+# subjects = {'english':89, 'maths': 96, 'science': 78}
+# subjects.update({'physics': 90, 'spanish':98})
+
+# print(subjects)
+
+
+
+#dictionaries
+#key value pairs
+
+#create a dictionary
+
+# cat = dict(name='kitty',age=5)
+
+# print(cat)
+# print(type(cat))
+
+# cat={'name':'kitty','age':5}
+
+# print(cat)
+
+#modify
+
+# cat={'name':'kitty','age':5}
+
+# print(cat['name'])
+
+# print(cat.get('gender','Not found'))
+ 
+# cat['age']=7
+
+# print(cat['age'])
+
+cat={'name':'kitty','age':5}
+# cat.pop('age')
+# print(cat)
+
+for i,j in cat.items():
+    print(i,j)
+
